79d4619b4869f4e3d6bcfedb028266cc37cf3a68
Azizbek Nosirov <abb923609@gmail.com>
2024-06-26 10:17:07 +0500

    done

---
+++ index.js
@@ -0,0 +1,32 @@
+fetch("https://fakestoreapi.com/products?limit=10")
+    .then((res) => res.json())
+    .then((data) => {
+        
+        let body = document.querySelector("body");
+
+        for (let i = 0; i < 10; i++) {
+            let prod = data[i];
+
+            let box = document.createElement("div");
+            let img = document.createElement("img");
+            let title = document.createElement("h3");
+            let rating = document.createElement("p");
+            let people = document.createElement("span");
+            let price = document.createElement("p");
+
+            img.src = prod.image;
+            title.textContent = prod.title;
+            rating.textContent = prod.rating.rate;
+            people.textContent = prod.rating.count;
+            price.textContent = prod.price;
+
+            box.appendChild(img);
+            box.appendChild(title);
+            box.appendChild(rating);
+            box.appendChild(people);
+            box.appendChild(price);
+
+            body.appendChild(box);
+            
+        };
+    });
